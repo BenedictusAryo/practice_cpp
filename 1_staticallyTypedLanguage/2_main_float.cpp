6d6eda813923d5c6e66762f5e397a2e72858b322
unknown <benedictusaryo@outlook.com>
2019-10-16 15:19:43 +0700

    practice float

--- 1_staticallyTypedLanguage/2_main_float.cpp
+++ 1_staticallyTypedLanguage/2_main_float.cpp
@@ -6,11 +6,16 @@ int main()
     // TODO: define two floating point numbers. Assign 12.07 to the
     // first floating point number. Assign 65.102 to the
     // second floating point number.
+    float first_float = 12.07;
+    float second_float = 65.102;
+    std::cout << "First Float variables: " << first_float << std::endl;
+    std::cout << "Second Float variables: " << second_float << std::endl;
 
     // TODO: Calculate the sum of the two floating point variables into
     // the float_sum variable.
 
     float float_sum;
+    float_sum = first_float + second_float;
     std::cout << float_sum << std::endl;
 
     // TODO: Calculate difference between the second and first number
