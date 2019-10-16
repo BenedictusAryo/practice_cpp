e08256882f65eddd48310fdee6db2b8c2c6bdf0b
unknown <benedictusaryo@outlook.com>
2019-10-16 14:12:17 +0700

    create new file

---
+++ 1_staticallyTypedLanguage/main_static.cpp
@@ -0,0 +1,23 @@
+// include all libraries needed
+#include <iostream>
+
+/* 	
+*	These are C++ comments. There are two ways to write comments in C++.
+*	Using the slash with an asterisk is one way.
+*/
+
+// Here is another way to write comments in C++
+
+/* In general, C++ code is run from a file called main.cpp
+* The implementation goes into a function called main().
+* The main() function almost always returns a zero, which provides evidence that 
+* the program ran to its end.
+*/
+
+//define main function
+int main()
+{
+
+    int integer_one;
+    integer_one = 5;
+}
