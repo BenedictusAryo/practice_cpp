361b0d861d8eb115ba3cc16c2e56da415e6f89b7
unknown <benedictusaryo@outlook.com>
2019-10-20 14:38:20 +0700

    fix the code

---
+++ 1_staticallyTypedLanguage/4_fixthecode.cpp
@@ -0,0 +1,39 @@
+/*
+*  Given this code, 
+*  Try to Fix the error
+*/
+
+// int main ()
+// {
+
+// 	x = 25
+// 	y = 61.4
+// 	z = 199.2
+
+// 	division = y / z
+
+// 	cout << x << '\n'
+// 	cout << y << '\n'
+// 	cout << division << '\n'
+
+// 	return 0
+// }
+
+// Answer:
+
+#include <iostream>
+
+int main()
+{
+    int x = 25;
+    float y = 61.4;
+    float z = 199.2;
+
+    float division = y / z;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << division << '\n';
+
+    return 0;
+}
